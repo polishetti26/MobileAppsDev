f4a2a181bad85b533b5b01603055ad65d33b73d6
pranaypolishetti26 <pranaypspk26@gmail.com>
2023-02-19 13:49:11 -0600

    2nd assignment

---
+++ a01-hello-world-master/app/src/main/java/com/ualr/firstapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.ualr.firstapp;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ualr.firstapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View rootView = mBinding.getRoot();
+        setContentView(rootView);
+    }
+
+    // TODO 06. Avoid updating the text label (userMsgTV) when the text field (userInputET) is empty
+    public void showTextMessage(View view) {
+        if(!mBinding.userInputET.getText().toString().isEmpty()){
+            mBinding.userMsgTV.setText(mBinding.userInputET.getText().toString());
+        }
+    }
+
+    public void cleanTextField(View view) {
+        mBinding.userInputET.setText("");
+    }
+
+
+    // TODO 07. Create a new method called cleanTextField to delete the text inside the text field
+}
